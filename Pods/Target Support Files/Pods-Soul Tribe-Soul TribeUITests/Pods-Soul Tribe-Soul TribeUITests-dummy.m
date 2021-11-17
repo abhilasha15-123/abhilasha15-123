53e66238bccbb3125f817c4a33f41ce4501368d6
abhilasha15-123 <94509925+abhilasha15-123@users.noreply.github.com>
2021-11-17 19:40:02 +0530

    Commit by abhilsha

---
+++ Pods/Target Support Files/Pods-Soul Tribe-Soul TribeUITests/Pods-Soul Tribe-Soul TribeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Soul_Tribe_Soul_TribeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Soul_Tribe_Soul_TribeUITests
+@end
---
+++ Pods/Target Support Files/Pods-Soul TribeTests/Pods-Soul TribeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Soul_TribeTests : NSObject
+@end
+@implementation PodsDummy_Pods_Soul_TribeTests
+@end
